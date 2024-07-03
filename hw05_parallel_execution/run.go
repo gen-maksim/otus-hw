6444e4c85406b6a462987c4f02aa9b978ce08abe
Генько Максим <genko.ma@gazprom-neft.ru>
2024-07-03 19:52:42 +0300

    finished hw!

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,69 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"time"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type ErrC struct {
+	mu sync.Mutex
+	i  int
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	wg := &sync.WaitGroup{}
+	schedule := make(chan Task, n)
+	quitCh := make(chan struct{})
+	errC := &ErrC{i: m}
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go work(wg, schedule, errC, quitCh)
+	}
+
+	defer wg.Wait()
+	defer close(schedule)
+
+	taskI := 0
+	for {
+		select {
+		case <-quitCh:
+			return ErrErrorsLimitExceeded
+		case schedule <- tasks[taskI]:
+			taskI++
+			if taskI == len(tasks) {
+				return nil
+			}
+		default:
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+}
+
+func work(wg *sync.WaitGroup, schedule chan Task, m *ErrC, quitCh chan struct{}) {
+	defer wg.Done()
+	for {
+		if m.i <= 0 {
+			select {
+			case <-quitCh:
+			default:
+				close(quitCh)
+			}
+			return
+		}
+		task, ok := <-schedule
+		if !ok {
+			break
+		}
+		err := task()
+		if err != nil {
+			m.mu.Lock()
+			m.i--
+			m.mu.Unlock()
+		}
+	}
 }
