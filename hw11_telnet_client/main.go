1a6ad94dac02cb161ec4ce942611e2ec7247a809
Генько Максим <genko.ma@gazprom-neft.ru>
2024-09-18 11:39:42 +0300

    started hw

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	timeout int
+)
+
+func init() {
+	flag.IntVar(&timeout, "timeout", 0, "timeout for connection")
+}
 func main() {
+	flag.Parse()
+	args := os.Args
+
+	if len(args) < 3 {
+		fmt.Println("Too few arguments")
+		return
+	}
+
+	url := args[1]
+	port := args[2]
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
