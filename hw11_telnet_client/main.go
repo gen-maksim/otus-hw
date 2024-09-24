dc937b7e0e1bec7407cdfd71d7d3d1e5b3f096c1
Генько Максим <genko.ma@gazprom-neft.ru>
2024-09-24 12:23:59 +0300

    made core logic

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -3,27 +3,51 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"sync"
+	"time"
 )
 
-var (
-	timeout int
-)
+var timeout string
 
 func init() {
-	flag.IntVar(&timeout, "timeout", 0, "timeout for connection")
+	flag.StringVar(&timeout, "timeout", "30s", "timeout for connection")
 }
+
 func main() {
 	flag.Parse()
 	args := os.Args
-
 	if len(args) < 3 {
-		fmt.Println("Too few arguments")
+		log.Println("Too few arguments")
 		return
 	}
 
-	url := args[1]
-	port := args[2]
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	url := args[2]
+	port := args[3]
+	in := os.Stdin
+	out := os.Stdout
+	duration, err := time.ParseDuration(timeout)
+	if err != nil {
+		log.Println("Invalid timeout")
+		return
+	}
+
+	c := NewTelnetClient(fmt.Sprintf("%v:%v", url, port), duration, in, out)
+	c.Connect()
+	defer c.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		c.Receive()
+	}()
+
+	go func() {
+		defer wg.Done()
+		c.Send()
+	}()
+
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -12,10 +16,75 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	Conn    net.Conn
+	closer  chan struct{}
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{in: in, out: out, timeout: timeout, address: address}
+}
+
+func (t *telnetClient) Connect() error {
+	dialer := &net.Dialer{}
+	ctx, cancel := context.WithTimeout(context.Background(), t.timeout)
+	conn, err := dialer.DialContext(ctx, "tcp", t.address)
+	if err != nil {
+		log.Fatalf("Cannot connect: %v", err)
+	}
+
+	closer := make(chan struct{})
+	go func() {
+		select {
+		case <-t.closer:
+		case <-ctx.Done():
+			cancel()
+			conn.Close()
+		}
+	}()
+	t.closer = closer
+	t.Conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *telnetClient) Close() error {
+	close(t.closer)
+	return nil
+}
+
+func (t *telnetClient) Send() error {
+	err := t.translate(t.in, t.Conn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *telnetClient) Receive() error {
+	err := t.translate(t.Conn, t.out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *telnetClient) translate(r io.Reader, w io.Writer) (err error) {
+	scanner := bufio.NewScanner(r)
+	for {
+		select {
+		case <-t.closer:
+			return nil
+		default:
+			if !scanner.Scan() {
+				return nil
+			}
+			text := scanner.Text()
+			w.Write([]byte(text + "\n"))
+		}
+	}
+}
