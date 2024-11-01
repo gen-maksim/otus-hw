0d0029c5ad0aa182a795cc0d56ada776c199ff72
Генько Максим <genko.ma@gazprom-neft.ru>
2024-11-01 11:59:17 +0300

    save intermediate changes

--- hw12_13_14_15_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware.go
@@ -1,11 +1,19 @@
 package internalhttp
 
 import (
+	"log"
 	"net/http"
 )
 
 func loggingMiddleware(next http.Handler) http.Handler { //nolint:unused
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO
+		log.Println(r.Host)
+		log.Println(r.RemoteAddr)
+		log.Println(r.Method)
+		log.Println(r.UserAgent())
+
+		next.ServeHTTP(w, r)
+		//log.Println(r.Response.Status)
+		//log.Println(r.Response.Body)
 	})
 }
--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -2,9 +2,14 @@ package internalhttp
 
 import (
 	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 )
 
-type Server struct { // TODO
+type Server struct {
+	server *http.Server // TODO
 }
 
 type Logger interface { // TODO
@@ -17,15 +22,32 @@ func NewServer(logger Logger, app Application) *Server {
 	return &Server{}
 }
 
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/hello" {
+
+		log.Printf("hello")
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode("Hi there!")
+	}
+}
+
 func (s *Server) Start(ctx context.Context) error {
-	// TODO
+	server := &http.Server{
+		Addr:              ":8080",
+		Handler:           loggingMiddleware(s),
+		ReadHeaderTimeout: 90 * time.Second,
+	}
+	s.server = server
+
+	server.ListenAndServe()
+
 	<-ctx.Done()
 	return nil
 }
 
 func (s *Server) Stop(ctx context.Context) error {
-	// TODO
+	s.server.Shutdown(ctx)
 	return nil
 }
-
-// TODO
